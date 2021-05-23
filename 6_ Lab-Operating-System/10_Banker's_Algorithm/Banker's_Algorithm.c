68ec3b47ef13623050202fd30970a8e0ca4bbe6d
Rahul Dabgotra <rahuldabgo@gmail.com>
2021-05-24 02:33:27 +0530

    Update Banker's_Algorithm.c

--- 6_ Lab-Operating-System/10_Banker's_Algorithm/Banker's_Algorithm.c
+++ 6_ Lab-Operating-System/10_Banker's_Algorithm/Banker's_Algorithm.c
@@ -1,34 +1,37 @@
 /* Implementation of Banker's Algorithm using C */
+
 #include <stdio.h>
 struct process
 {
     int all[6], max[6], need[6], finished, request[6], c[6];
 } p[10];
+
 int avail[6], sseq[10], ss = 0, check1 = 0, check2 = 0, m, n, pid, work[6];
+
 void main()
 {
     int safeseq(void);
-    int ch, i = 0, j = 0, pid, ch1;
+    int ch, i = 0, j = 0, k, pid, ch1;
     int vcheck = 0, wcheck = 0;
     do
     {
-        printf("\n Menu: \n 1.Input \n 2.New Request \n 3.Safe State or Not \n 4.Exit");
-        printf("\n Enter your choice :");
+        printf("\nMenu: \n 1) Input   2) New Request   3)Safe State or Not   4) Exit");
+        printf("\nEnter your choice (1-4) :");
         scanf("%d", &ch);
 
         switch (ch)
         {
         case 1:
-            printf("\n Enter the number of processes: ");
+            printf("\nEnter the number of processes: ");
             scanf("%d", &n);
-            printf("\n Enter the number of resources: ");
+            printf("\nEnter the number of resources: ");
             scanf("%d", &m);
-            printf("\n Enter the max unit of each resource: ");
+            printf("\nEnter the max unit of each resource: \n");
             for (j = 0; j < m; j++)
             {
                 scanf("%d", &avail[j]);
             }
-            printf("\n Enter max matrix:");
+            printf("\nEnter max matrix: \n");
             for (i = 0; i < n; i++)
             {
                 for (j = 0; j < m; j++)
@@ -36,7 +39,7 @@ void main()
                     scanf("%d", &p[i].max[j]);
                 }
             }
-            printf("\n Enter Allocation matrix :\n");
+            printf("\nEnter allocation matrix : \n");
             for (i = 0; i < n; i++)
             {
                 for (j = 0; j < m; j++)
@@ -56,7 +59,7 @@ void main()
         case 2:
             vcheck = 0;
             wcheck = 0;
-            printf("\n\n\t Requesting Process ID:");
+            printf("\n\tRequesting Process ID:");
             scanf("%d", &pid);
             printf("Enter the request of process \n");
             for (j = 0; j < m; j++)
@@ -69,9 +72,9 @@ void main()
                     wcheck = 1;
             }
             if (vcheck == 1)
-                printf("\n\n\t The process exceeds its max need: Terminated");
+                printf("\n\tThe process exceeds its max need: Terminated");
             else if (wcheck == 1)
-                printf("\n\n\t Lack of resources : Process state is Wait");
+                printf("\n\tLack of resources : Process state is Wait");
             else
             {
                 for (j = 0; j < m; j++)
@@ -85,15 +88,16 @@ void main()
             break;
         case 3:
             if (safeseq() == 1)
-                printf("\n\n\t The system is in safe state");
+                printf("\n\tThe system is in safe state");
             else
-                printf("\n\n\t The system is not in safe state");
+                printf("\n\tThe system is not in safe state");
             break;
         case 4:
             break;
         } // end of switch loop
     } while (ch != 4);
 }
+
 int safeseq()
 {
     int tj, tk, i, j, k;
@@ -112,8 +116,8 @@ int safeseq()
             {
                 check1 = 0;
                 for (k = 0; k < m; k++)
-                    if (p[j].need[k] <= work[k])
-                        check1++;
+                if (p[j].need[k] <= work[k])
+                    check1++;
 
                 if (check1 == m)
                 {
@@ -132,14 +136,14 @@ int safeseq()
     for (i = 0; i < n; i++)
         if (p[i].finished = 1)
             check2++;
-    if (check2 > n)
+    if (check2 >= n)
     {
-        printf("\n\n\t The system is in safe state");
+        printf("\n\tThe system is in safe state ");
         for (tj = 0; tj < n; tj++)
-            printf("p%d,", sseq[tj]);
+            printf("P%d,", sseq[tj]);
         return 1;
     }
     else
-        printf("\n\n\t The system is not in safe state");
+        printf("\n\tThe system is NOT in safe state");
     return 0;
 }
